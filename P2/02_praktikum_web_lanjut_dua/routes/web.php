302eeafa8f7e3210f0c9e7e053bea7dd3225013f
= <=>
2022-02-21 22:20:13 +0700

    Praktikum2

--- P2/02_praktikum_web_lanjut_dua/routes/web.php
+++ P2/02_praktikum_web_lanjut_dua/routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +17,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', [PageController::class, 'index'] );
+// Route::get('/about', [PageController::class, 'about'] );
+// Route::get('/articles/{id}', [PageController::class, 'articles'] );
+
+Route::get('/', [HomeController::class, 'index'] );
+Route::get('/about', [AboutController::class, 'about'] );
+Route::get('/articles/{id}', [ArticleController::class, 'articles'] );
